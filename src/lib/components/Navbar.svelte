3f0653ff3e84f523b547b170bb2f2678d8eee2ae
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2023-07-18 18:59:44 +0700

    initial commit

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,10 @@
+<nav
+    class="h-16 bg-white drop-shadow-xl p-4"
+>
+    <div class="container mx-auto flex flex-row justify-between items-center">
+        <h1 class="text-2xl font-bold">Phatsanphon's Gallery</h1>
+        <div>
+            Hello World
+        </div>
+    </div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,14 @@
-<script>
+<script lang="ts">
     import "../app.css";
+    import Navbar from "$lib/components/Navbar.svelte";
 </script>
 
-<slot />
+<Navbar />
+
+<head>
+    <link href="https://cdn.jsdelivr.net/gh/lazywasabi/thai-web-fonts@7/fonts/LINESeedSansTH/LINESeedSansTH.css" rel="stylesheet" />
+</head>
+
+<main class="container mx-auto pt-4">
+    <slot />
+</main>
